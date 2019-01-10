6f8c4e59763b81b1a1ac900f10cd875857f61f8c
Anastasia Uzhentseva <uzhentseva@yahoo.com>
2019-01-10 16:44:05 +0300

    1

--- vkontakte/Friends/FriendsController.swift
+++ vkontakte/Friends/FriendsController.swift
@@ -55,66 +55,28 @@ class FriendsController: UITableViewController {
     }
     
     
+    
     @IBAction func logout(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if segue.identifier == "friendToPic" {
-//
-//            let photoFriendsController : MyFriendsCollectionController = segue.destination as! MyFriendsCollectionController
-//            let myFriendsController = segue.source as! FriendsController
-//
-//            //  Получаем индекс выделенной ячейки
-//            if let indexPath = myFriendsController.tableView.indexPathsForSelectedRows {
-//
-//                // Получаем друга по индексу
-//                let friend = myFriendsController.friendsArray[indexPath[0].row]
-//                if let fotoDelegate = myFriendsController.friendsPicArray[friend] as? [String] {
-//                    
-//                    photoFriendsController.fotoDelegate = fotoDelegate
-//                    print(photoFriendsController.fotoDelegate)
-//                }
-//            }
-//        }
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "friendToPic" {
+            
+            if let indexPath = tableView.indexPathForSelectedRow {
+                
+                let friend = friendsArray[indexPath.row]
+                let image = friendsPicArray[indexPath.row]
+                
+                let secondVC: MyFriendsCollectionController = segue.destination as! MyFriendsCollectionController
+                
+                secondVC.myFriendNames = friend
+                secondVC.myFriendPicture = image
+                
+                
+            }
+        }
+    }
+    
+    
 }
-
-
-/*
- // Override to support editing the table view.
- override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
- if editingStyle == .delete {
- // Delete the row from the data source
- tableView.deleteRows(at: [indexPath], with: .fade)
- } else if editingStyle == .insert {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
- 
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
- // Return false if you do not want the item to be re-orderable.
- return true
- }
- */
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
-
--- vkontakte/MyGroups/MyGroupsTableController.swift
+++ vkontakte/MyGroups/MyGroupsTableController.swift
@@ -16,11 +16,6 @@ class MyGroupsTableController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
@@ -55,31 +50,6 @@ class MyGroupsTableController: UITableViewController {
         }
     }
 
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func addGroup(segue: UIStoryboardSegue) {
         if segue.identifier == "addGroup" {
 
